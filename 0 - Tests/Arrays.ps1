9db093672414e9d4c71d55bfe8dcfa50ef1c65b7
Milli <onethenumbernottheword@gmail.com>
2022-05-01 21:44:02 +0200

    Test for arrays

---
+++ 0 - Tests/Arrays.ps1
@@ -0,0 +1,15 @@
+﻿Push-Location $PSScriptRoot
+$ParentFolder = (get-item $PSScriptRoot).parent.FullName
+Import-Module -name "$ParentFolder\Modules\modules.psm1" -Force
+ 
+Function Modify-Array([string[]] $array) {
+    $array[1] = 4
+}
+
+$array = @(1,2,3)
+Modify-Array $array
+Write-Host $array
+
+$array = @(@(1,2),@(2,3))
+$flat = $(Flatten -object $array)
+$group = $flat | Group-Object | Sort-Object -Property Count
